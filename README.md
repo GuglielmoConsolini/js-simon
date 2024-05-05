2a1375c981c4f0a9a96520551c70b9b5f727b02d
GuglielmoConsolini <consoliniguglielmo@gmail.com>
2024-05-05 23:07:24 +0200

    Milestone 3 e 4

--- README.md
+++ README.md
@@ -14,3 +14,20 @@ MILESTONE 2
 1 Setto un timer che alla fine della sua durata fara scomparire l'array di numeri
 
 2 seleziono l'elemento contenente i numeri e gli dico che il suo innerhtml allo scadere del timer sarà una stringa vuota.
+
+
+
+MILESTONE 3
+
+1 Dentro la funzione anonima dichiaro un altro array , e con un ciclo while faccio apparir un prompt che chiede all'utente di inserire i numeri che ha visto
+
+2 Pusho i numeri inseriti dall'utente dentro l'array.
+
+3 Lo stampo in console 
+
+
+
+
+Milestone 4
+
+1 Per confrontare i due array utilizzo la funzione "filter" , che mi segnalerà i numeri in comune fra i due array e li inserirà in una variabile , che successivament andrò a stampare nel DOM
--- js/main.js
+++ js/main.js
@@ -1,26 +1,36 @@
 
+
+let numeriCasuali
+
+// Genera i numeri casuali subito
+numeriCasuali = generaNumeri(1, 100, 5); 
+console.log(generaNumeri(1,100,5))
+document.getElementById("numbers-box").innerHTML = "Numeri casuali: " + numeriCasuali.join(", ");
+
+// Attendi 3 secondi prima di mostrare i prompt
 setTimeout(function(){
-    console.log("ciao")
-    document.getElementById("numbers-box").innerHTML = " "
-}, 3000)
+    let numeriUtente = [];
+    while(numeriUtente.length < 5) {
+        let numero = prompt("Inserisci un numero:");
+        numeriUtente.push(Number(numero));
+    }
+    console.log("Numeri inseriti dall'utente:", numeriUtente);
+    
+    // Confronto degli array
+    let numeriIndovinati = numeriCasuali.filter(numero => numeriUtente.includes(numero));
+    console.log("Numeri indovinati:", numeriIndovinati);
+    
+    document.getElementById("numbers-box").innerHTML = "numeri indovinati: " + numeriIndovinati.join(", ");
+},10*3000);
 
- function generaNumeri(minimo, massimo , quantità) {
-
-let numeriGenerati = []
-
-for(let i = 0 ; i < quantità ; i++){
-
-let numeroCasuale = Math.floor(Math.random() * (massimo - minimo + 1)) + minimo;
-numeriGenerati.push(numeroCasuale);
-
+// funzione genera numeri
+function generaNumeri(minimo, massimo, quantità) {
+    let numeriGenerati = [];
+    for (let i = 0; i < quantità; i++) {
+        let numeroCasuale = Math.floor(Math.random() * (massimo - minimo + 1)) + minimo;
+        numeriGenerati.push(numeroCasuale);
+    }
+    return numeriGenerati;
 }
-return numeriGenerati;
-}
-
-numeriGenerati = generaNumeri(1 , 100 , 5);
-console.log(numeriGenerati)
-
-document.getElementById("numbers-box").innerHTML = "Ricorda questi numeri: " + numeriGenerati;
-   
  
 
